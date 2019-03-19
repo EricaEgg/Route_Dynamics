3eeae39ab22dba6224937e078929cf5455ec208b
tkm22 <46464882+tkm22@users.noreply.github.com>
2019-03-19 15:58:17 -0700

    setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+setup(
+        name = 'Route_Dynamics',
+        version = '1.0',
+        description='Setup for Route_Dynamics',   
+        author='xxx',  
+        author_email='123@uw.edu',  
+        url='https://github.com/EricaEgg/Route_Dynamics',     
+        packages=['Route_Dynamics'], 
+        package_dir={"project" : 'Route_Dynmaics'}
+)
