e5d7ffd2bd08c48a85b875890563a50cc8988fb6
Erica Eggleton <egglee@uw.edu>
2019-03-21 00:19:40 -0700

    adding test for multiple routes

---
+++ route_dynamics/Test/test_multiple_route.py
@@ -0,0 +1,15 @@
+import base
+import single_route
+import multiple_route
+
+def test_routes_analysis_ranking():
+    
+    route_list = [40, 45]
+    shapefile = 'six_routes.shp'
+    rasterfile = 'seattle_dtm.tif'
+    
+    temp = multiple_route.routes_analysis_ranking(route_list, shapefile, rasterfile)
+    
+    assert type(temp) == matplotlib.axes._subplots.AxesSubplot
+
+    return
