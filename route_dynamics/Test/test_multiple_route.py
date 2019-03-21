19b26cab257d8489048f2e1c5b6db91af58a0cc7
aidowu <atinukeidowu@gmail.com>
2019-03-21 00:51:41 -0700

    Added docstrings to the multi_route files

--- route_dynamics/Test/test_multiple_route.py
+++ route_dynamics/Test/test_multiple_route.py
@@ -2,8 +2,11 @@ import base
 import single_route
 import multiple_route
 
+
 def test_routes_analysis_ranking():
-    
+    """
+       Test that the output is a matplotlib plot
+    """
     route_list = [40, 45]
     shapefile = 'six_routes.shp'
     rasterfile = 'seattle_dtm.tif'
--- route_dynamics/multiple_route.py
+++ route_dynamics/multiple_route.py
@@ -4,6 +4,20 @@ import base
 
 
 def routes_analysis_ranking(route_list, shapefile, rasterfile):
+    """
+    Computes the four chosen metrics values for each bus routes in route_list and display them on a bar plot for
+    ease of comparison
+
+    Parameters
+    ----------
+    route_list: A list of bus routes to be compared (Integers)
+    shapefile: route geospatial data (.shp file)
+    rasterfile: elevation data file (.tif)
+
+    Returns
+    -------
+    bar plot: showing results comparison between bus routes according to the four metrics chosen
+    """
     num_route = len(route_list)
     metrics_1 = [0] * num_route
     metrics_2 = [0] * num_route
@@ -11,8 +25,6 @@ def routes_analysis_ranking(route_list, shapefile, rasterfile):
     metrics_4 = [0] * num_route
     for idx, route_num in enumerate(route_list):
         route_shp = base.read_shape(shapefile, route_num)
-    
-        linestring_route_df = base.extract_point_df(route_shp)
     
         elevation, elevation_gradient, route_cum_distance, distance = base.gradient(route_shp, rasterfile)
     
