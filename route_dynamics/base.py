cdef40416beb203ed067dddde67d3911c0dfa03e
Harrison J Goldwyn <goldwyn@uw.edu>
2019-04-23 16:39:18 -0700

    Removed erronious 'abs' in calculation of gradient.

--- route_dynamics/base.py
+++ route_dynamics/base.py
@@ -152,7 +152,16 @@ def gradient(route_shp, rasterfile):
         # the sign of the gradient would determine if we the bus is
         # going up or down a hill. I do see that we get back the right
         # length list
-    route_gradient =  np.insert(abs(np.diff(elevation) / route_distance), 0, 0)
+    route_gradient =  np.insert(
+        (
+            np.diff(elevation)
+            /
+            route_distance
+            )
+        ,
+        0,
+        0,
+        )
 
     return elevation_meters, route_gradient, route_cum_distance, route_distance
 
@@ -312,7 +321,11 @@ def route_metrics(
         """
 
     metrics_1 = 100 * sum(elevation_gradient)/ max(route_cum_distance)
-    metrics_2 = sum(abs(np.diff(elevation[0])))/ max(route_cum_distance)
+    metrics_2 = sum(
+        abs(
+            np.diff(elevation[0])
+            )
+        )/ max(route_cum_distance)
     metrics_3 = 100 * (
         sum(
             np.insert(
