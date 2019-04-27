3604161f3996d5a9855d1f604153ea4022b6df72
Harrison J Goldwyn <goldwyn@uw.edu>
2019-04-27 08:59:08 -0700

    Changed base imports to relative 'from . import base'

--- route_dynamics/route_elevation/multiple_route.py
+++ route_dynamics/route_elevation/multiple_route.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-import base
+from . import base
 
 
 def routes_analysis_ranking(route_list, shapefile, rasterfile):
@@ -25,16 +25,16 @@ def routes_analysis_ranking(route_list, shapefile, rasterfile):
     metrics_4 = [0] * num_route
     for idx, route_num in enumerate(route_list):
         route_shp = base.read_shape(shapefile, route_num)
-    
+
         elevation, elevation_gradient, route_cum_distance, distance = base.gradient(route_shp, rasterfile)
-    
+
         _ , metrics = base.route_metrics(elevation, elevation_gradient, route_cum_distance, distance, route_num)
-    
+
         metrics_1[idx], metrics_2[idx], metrics_3[idx], metrics_4[idx] = metrics
 
     data = pd.DataFrame({'Bus Num': route_list, 'M1': metrics_1, 'M2': metrics_2, 'M3': metrics_3, 'M4': metrics_4})
     ax = data.plot.bar('Bus Num', figsize= [14, 5], fontsize= 20)
     ax.set_ylabel('Metrics', size= 20)
     ax.set_xlabel('Bus Number', size= 20)
-    
-    return ax    
+
+    return ax
