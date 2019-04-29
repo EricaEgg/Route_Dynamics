765a4c791344dcd1b56ba1893827a272aadaa582
Harrison J Goldwyn <goldwyn@uw.edu>
2019-04-29 11:43:58 -0700

    Moved tests and got them working again with 'pytest' from the command line.

--- route_dynamics/tests/test_multiple_route.py
+++ route_dynamics/tests/test_multiple_route.py
@@ -4,11 +4,11 @@ from os import path
 import sys
 sys.path.append(path.abspath('..'))
 
-import base
-import multiple_route
+from ..route_elevation import base
+from ..route_elevation import multiple_route
 
-shapefile = '../../data/six_routes.shp'
-rasterfile = '../../data/seattle_dtm.tif'
+shapefile = '../data/six_routes.shp'
+rasterfile = '../data/seattle_dtm.tif'
 
 
 def test_routes_analysis_ranking():
@@ -16,9 +16,9 @@ def test_routes_analysis_ranking():
        Test that the output is a matplotlib plot
     """
     route_list = [40, 45]
-    
+
     temp = multiple_route.routes_analysis_ranking(route_list, shapefile, rasterfile)
-    
+
     assert rasterfile.endswith('.tif'), 'Rasterfile should be .tif.'
     assert shapefile.endswith('.shp'), 'Shapefile should be .shp.'
 
---
+++ route_dynamics/tests/test_route_power.py
@@ -0,0 +1,19 @@
+""" Test for route_energy
+	"""
+
+class TestRouteDataFrame(object):
+    """ Constructs route dataframe given the arguments;
+
+        Args:
+            route points (x,y)
+            elevation (z)
+            velocity
+
+        returns: GeoDataFrame object.
+        """
+    def test_one(self):
+        assert 1 == 1
+
+    def test_two(self):
+
+        assert 1 == 2
