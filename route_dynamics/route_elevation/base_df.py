426bc408a75de25e342afeab3d569824249eea35
Erica Eggleton <egglee@uw.edu>
2021-11-10 15:11:27 -0800

    Updated base_df package so it is importable into dev nb

--- route_dynamics/route_elevation/base_df.py
+++ route_dynamics/route_elevation/base_df.py
@@ -7,13 +7,14 @@ import matplotlib.pyplot as plt
 from scipy.signal import savgol_filter as sf
 
 
-def create_gdf(shapefile):
+def create_gdf(shapefile, frequency):
     """
-        Loads shapefile into GeoDataFrame 
+        Loads shapefile into GeoDataFrame with original point distance
 
         Parameters
         ----------
         shapefile: route geospatial data (.shp file)
+        frequency: distance between stops (in ft)
 
         Returns
         -------
@@ -26,41 +27,53 @@ def create_gdf(shapefile):
 
     for i in route_df.index:
     
-        x.append(i*6) # 6 is to indicate points are 6 ft apart
+        x.append(i*frequency)
     
 
-    route_df['length'] = x
+    route_df['distance'] = x
     route_df = route_df.drop(columns=['Id','ORIG_FID', 'SHAPE_Leng'])
     
     return route_df
 
+def metric_df(df):
+    df = df.iloc[::6]
+    df = df.reset_index()
+    df = df[['geometry', 'distance', 'Z']]
+    df = df.rename(columns = {'Z':'elevation'})
+    df['distance'] *= 0.3048
+    df['elevation'] *= 0.3048
+    
+    return df
+
 
 def filters(route_df):
     
-    points = route_df[('Z')].values
+    points = route_df[('elevation')].values
 
-    y_new = sf(points, 439, 3,  axis = 0) 
+    y_new = sf(points, 43, 3,  axis = 0) 
     
     return y_new
 
 
-def grade(rt_df, y_new):
+def grade(y_new):
     
     grade_SG = [0]
 
     for i in range(1,len(y_new)):
-        grade_SG.append((y_new[i]-y_new[i-1])/6)
+        grade_SG.append((y_new[i]-y_new[i-1])/10.97)
     
     return grade_SG
 
 
 def wrapper(shapefile):
 
-	route_df = create_gdf(shapefile)
+    route_df = create_gdf(shapefile, 6)
+    
+    route_df = metric_df(route_df)
 
-	route_df['Z'] = filters(route_df)
+    route_df['elevation'] = filters(route_df)
 
-	route_df['grade'] = grade(route_df, route_df['Z'])
+    route_df['grade'] = grade(route_df['elevation'].values)
 
-	return route_df
+    return route_df
 
